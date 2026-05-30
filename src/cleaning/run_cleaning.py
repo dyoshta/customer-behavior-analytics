d43c4940f2a775e7db01d90e845e43a8746ca4f1
ujoir <ujoir@student.kit.edu>
2026-05-30 14:41:39 +0200

    updated run_cleaning()

--- src/cleaning/run_cleaning.py
+++ src/cleaning/run_cleaning.py
@@ -1,4 +1,5 @@
-from cleaning.preprocessing import normalize_column_names, remove_duplicates, handle_missing
+from cleaning.preprocessing import normalize_column_names, remove_duplicates, handle_missing, remove_invalid_rows, \
+    fix_datatypes
 from config import PROCESSED_DATA_PATH
 
 
@@ -10,6 +11,7 @@ def run_cleaning(df):
     1. Normalize column names;
     2. Remove duplicates;
     3. Remove rows with missing customer_id value;
+    4. Remove rows with invalid transactions (quantity <= 0 & unit_price <= 0)
 
     :param df: raw dataframe.
     :return: cleaned dataframe (df).
@@ -17,5 +19,7 @@ def run_cleaning(df):
     df = normalize_column_names(df)
     df = remove_duplicates(df)
     df = handle_missing(df)
+    df = remove_invalid_rows(df)
+    df = fix_datatypes(df)
     return df
 
